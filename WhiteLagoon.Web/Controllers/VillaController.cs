d44dcd01010a5fd632c58097c8cfe201f7f052d8
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-01-12 11:27:21 +0530

    Added temp data notifications for success and error while performing crud operations.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -33,6 +33,7 @@ namespace WhiteLagoon.Web.Controllers
             {
                 _db.Villas.Add(villa);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -54,6 +55,7 @@ namespace WhiteLagoon.Web.Controllers
             {
                 _db.Villas.Update(villa);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -77,8 +79,10 @@ namespace WhiteLagoon.Web.Controllers
             {
                 _db.Villas.Remove(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
                 return RedirectToAction("Index");
             }
+            TempData["error"] = "The villa could not be deleted.";
             return View();
         }
     }
