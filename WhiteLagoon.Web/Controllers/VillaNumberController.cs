62ec30934e994df920cff89f7d4a85917661d647
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-01-12 23:14:06 +0530

    Added Create VillaNumber and Index view for Villa Numbers list

---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.VillaNumbers.ToList();
+            return View(data);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber villaNumber)
+        {
+           // ModelState.Remove("Villa");
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(villaNumber);
+                _db.SaveChanges();
+                TempData["success"] = "The villa number has been created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(u=>u.Id==villaId);
+            if (villa == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa villa)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(villa);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int villaId)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(d => d.Id == villaId);
+            if (villa == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(d => d.Id == villa.Id);
+
+            if (obj is not null)
+            {
+                _db.Villas.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa could not be deleted.";
+            return View();
+        }
+    }
+}
