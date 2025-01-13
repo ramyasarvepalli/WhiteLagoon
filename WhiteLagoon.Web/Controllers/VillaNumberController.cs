3e6eade4fe71a38c054c9bfca7d2a5d2832c65b5
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-01-13 18:54:41 +0530

    Used Include method to refer Villas in VillaNumbers - to retreive VillaName instead of VillaId in UI.

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 using WhiteLagoon.Web.Models.ViewModels;
@@ -15,7 +16,7 @@ namespace WhiteLagoon.Web.Controllers
         }
         public IActionResult Index()
         {
-            var data = _db.VillaNumbers.ToList();
+            var data = _db.VillaNumbers.Include(u=>u.Villa).ToList();
             return View(data);
         }
 
