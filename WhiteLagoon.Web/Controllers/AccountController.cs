3ca9c9caacb01b5fb12711553c587f265521c5cc
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-02-21 17:33:14 +0530

    Add roles - Customer and Admin in Register page using RoleManager helper methods.

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -31,6 +31,11 @@ namespace WhiteLagoon.Web.Controllers
         }
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
             return View();
         }
     }
