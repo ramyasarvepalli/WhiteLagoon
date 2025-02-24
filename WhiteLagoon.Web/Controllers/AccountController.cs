297309da50656b0682c5d1a4a5b023aeb92cb3df
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-02-24 12:13:24 +0530

    Created post method for Login

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -31,6 +31,31 @@ namespace WhiteLagoon.Web.Controllers
             };
             return View(loginVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(loginVM.RedirectUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                }
+            }
+            return View(loginVM);
+        }
         public IActionResult Register()
         {
             if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
